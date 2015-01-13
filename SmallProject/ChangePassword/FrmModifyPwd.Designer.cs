5108bc693b452d100bde6aac66880a43aa2bd599
foolrabbit <373393875@qq.com>
2015-01-13 11:02:47 +0800

    ADO.NET(Access)修改密码示例

---
+++ SmallProject/ChangePassword/FrmModifyPwd.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ChangePassword
+{
+    partial class frmModifyPwd
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblOldPwd = new System.Windows.Forms.Label();
+            this.lblNewPwd = new System.Windows.Forms.Label();
+            this.lblConfirmPwd = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtOldPwd = new System.Windows.Forms.TextBox();
+            this.txtNewPwd = new System.Windows.Forms.TextBox();
+            this.txtConfirmPwd = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(52, 50);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(41, 12);
+            this.lblUserName.TabIndex = 0;
+            this.lblUserName.Text = "用户名";
+            // 
+            // lblOldPwd
+            // 
+            this.lblOldPwd.AutoSize = true;
+            this.lblOldPwd.Location = new System.Drawing.Point(52, 89);
+            this.lblOldPwd.Name = "lblOldPwd";
+            this.lblOldPwd.Size = new System.Drawing.Size(41, 12);
+            this.lblOldPwd.TabIndex = 1;
+            this.lblOldPwd.Text = "旧密码";
+            // 
+            // lblNewPwd
+            // 
+            this.lblNewPwd.AutoSize = true;
+            this.lblNewPwd.Location = new System.Drawing.Point(52, 126);
+            this.lblNewPwd.Name = "lblNewPwd";
+            this.lblNewPwd.Size = new System.Drawing.Size(41, 12);
+            this.lblNewPwd.TabIndex = 2;
+            this.lblNewPwd.Text = "新密码";
+            // 
+            // lblConfirmPwd
+            // 
+            this.lblConfirmPwd.AutoSize = true;
+            this.lblConfirmPwd.Location = new System.Drawing.Point(52, 163);
+            this.lblConfirmPwd.Name = "lblConfirmPwd";
+            this.lblConfirmPwd.Size = new System.Drawing.Size(53, 12);
+            this.lblConfirmPwd.TabIndex = 3;
+            this.lblConfirmPwd.Text = "确认密码";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(117, 47);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 21);
+            this.txtUserName.TabIndex = 4;
+            // 
+            // txtOldPwd
+            // 
+            this.txtOldPwd.Location = new System.Drawing.Point(117, 86);
+            this.txtOldPwd.Name = "txtOldPwd";
+            this.txtOldPwd.PasswordChar = '*';
+            this.txtOldPwd.Size = new System.Drawing.Size(100, 21);
+            this.txtOldPwd.TabIndex = 5;
+            // 
+            // txtNewPwd
+            // 
+            this.txtNewPwd.Location = new System.Drawing.Point(117, 123);
+            this.txtNewPwd.Name = "txtNewPwd";
+            this.txtNewPwd.PasswordChar = '*';
+            this.txtNewPwd.Size = new System.Drawing.Size(100, 21);
+            this.txtNewPwd.TabIndex = 6;
+            // 
+            // txtConfirmPwd
+            // 
+            this.txtConfirmPwd.Location = new System.Drawing.Point(117, 160);
+            this.txtConfirmPwd.Name = "txtConfirmPwd";
+            this.txtConfirmPwd.PasswordChar = '*';
+            this.txtConfirmPwd.Size = new System.Drawing.Size(100, 21);
+            this.txtConfirmPwd.TabIndex = 7;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(54, 206);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 8;
+            this.btnOK.Text = "确定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(142, 206);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "重置";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // frmModifyPwd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtConfirmPwd);
+            this.Controls.Add(this.txtNewPwd);
+            this.Controls.Add(this.txtOldPwd);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblConfirmPwd);
+            this.Controls.Add(this.lblNewPwd);
+            this.Controls.Add(this.lblOldPwd);
+            this.Controls.Add(this.lblUserName);
+            this.Name = "frmModifyPwd";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "修改密码";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmModifyPwd_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblOldPwd;
+        private System.Windows.Forms.Label lblNewPwd;
+        private System.Windows.Forms.Label lblConfirmPwd;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtOldPwd;
+        private System.Windows.Forms.TextBox txtNewPwd;
+        private System.Windows.Forms.TextBox txtConfirmPwd;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ SmallProject/ChangePassword/FrmModifyPwd.cs
@@ -0,0 +1,175 @@
+﻿namespace ChangePassword
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Text;
+    using System.Windows.Forms;
+    using System.Data.OleDb;
+
+    public partial class frmModifyPwd : Form
+    {
+        #region Fields
+
+        /// <summary>
+        /// 数据源连接
+        /// </summary>
+        private OleDbConnection _Con;
+
+        #endregion
+
+        public frmModifyPwd()
+        {
+            InitializeComponent();
+        }
+
+        #region Methods
+
+        #region EventFun
+
+        /// <summary>
+        /// 确定方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (txtNewPwd.Text != txtConfirmPwd.Text)
+            {
+                ShowMessage("两次输入的新密码不一致，无法修改密码。");
+                return;
+            }
+
+            try
+            {
+                OpenConnection();
+                DataTable dt = QueryData(string.Format("select 1 from [User] where [USERNAME]='{0}' and [PASSWORD]='{1}'", txtUserName.Text, txtOldPwd.Text));
+                if (dt.Rows.Count < 1)
+                {
+                    ShowMessage("原用户名和密码错误，无法修改密码。");
+                    return;
+                }
+                if (ExecuteCommand(string.Format("update [User] set [PASSWORD]='{1}' where [USERNAME]='{0}'", txtUserName.Text, txtNewPwd.Text)) == true)
+                {
+                    ShowMessage("修改密码成功。");
+                    return;
+                }
+                else
+                {
+                    ShowMessage("修改密码失败。");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        /// <summary>
+        /// 重置方法
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtUserName.Clear();
+            txtOldPwd.Clear();
+            txtNewPwd.Clear();
+            txtConfirmPwd.Clear();
+        }
+
+        /// <summary>
+        /// 窗体关闭
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmModifyPwd_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CloseConnection();
+        }
+
+        #endregion
+
+        #region Private
+
+        /// <summary>
+        /// 显示消息
+        /// </summary>
+        /// <param name="msg"></param>
+        private void ShowMessage(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        /// <summary>
+        /// 打开数据连接
+        /// </summary>
+        private void OpenConnection()
+        {
+            if (_Con == null)
+            {
+                _Con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=.\MyData.accdb;Persist Security Info=False");
+            }
+            _Con.Open();
+        }
+
+        /// <summary>
+        /// 关闭数据连接
+        /// </summary>
+        private void CloseConnection()
+        {
+            if (_Con != null)
+            {
+                if (_Con.State == ConnectionState.Open)
+                {
+                    _Con.Close();
+                }
+                _Con = null;
+            }
+        }
+
+        /// <summary>
+        /// 查询数据
+        /// </summary>
+        /// <param name="cmdText"></param>
+        /// <returns></returns>
+        private DataTable QueryData(string cmdText)
+        {
+            DataTable dt = new DataTable();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmdText, _Con);
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// 执行命令
+        /// </summary>
+        /// <param name="cmdText"></param>
+        /// <returns></returns>
+        private bool ExecuteCommand(string cmdText)
+        {
+            OleDbCommand cmd = new OleDbCommand(cmdText, _Con);
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        #endregion
+
+
+    }
+}
